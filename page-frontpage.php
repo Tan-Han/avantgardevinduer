068340067c19dcfa9e5a1797627d3afc9324b4e5
Tanya <tanhan01@easv365.dk>
2023-11-22 11:32:40 +0100

    Frontpage

--- page-frontpage.php
+++ page-frontpage.php
@@ -1,6 +1,28 @@
 <?php get_header() ?>
 
-<h1> Frontpage BABY </h1>
+<div class="hero">
+  <img src="img\Placeholder.JPG" alt="">
+  <div class="hero-text">
+    <h1> Avantgardevinduer </h1>
 
+    <hr>
+
+    <h3>Lavenergi til konkurrencedygtige priser</h3>
+  </div>
+</div>
+
+<div class="frontpage-mini-nav">
+  <p>Vinduer i plast, plast-alu, træ, træ-alu & alu</p>
+</div>
+
+<div class="content">
+  <div class="text-box">
+    <h2 class="company-name">Avantgardevinduer & Døre</h2>
+    <p class="company-description">Firma beskrivelse</p>
+  </div>
+  <div class="image-box">
+    <img src="img\Placeholder.JPG" alt="">
+  </div>
+</div>
 
 <?php get_footer() ?>
