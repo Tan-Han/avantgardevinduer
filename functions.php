2527c419f884ffa44e77d053d65a49297ba220fb
Tanya <tanhan01@easv365.dk>
2023-11-22 12:02:12 +0100

    Update functions link to style

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
-function enqueue_parent_theme_style()
-{
-    wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
-}
-add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
-
+  function mt_register_stylesheets() {
+    wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
+  }
+  add_action( "wp_enqueue_scripts", "mt_register_stylesheets" );
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avantgardevinduer & Døre</title>
-    <link rel="stylesheet" href="/style.css">
     <?php wp_head() ?>
 </head>
 <body>
