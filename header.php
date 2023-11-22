3908b1a7b2e89db6aa6eb13b77365cac7cf5ee95
Tanya <tanhan01@easv365.dk>
2023-11-22 15:09:48 +0100

    Navbar

--- header.php
+++ header.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.typekit.net/tmr5ico.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Avantgardevinduer & Døre</title>
     <?php wp_head() ?>
 </head>
+
 <body>
+
+    <div class="nav">
+        <input type="checkbox" id="nav-check">
+        <div class="nav-header">
+            <div class="nav-title">
+                Avantgardevinduer
+            </div>
+        </div>
+        <div class="nav-btn">
+            <label for="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+
+        <div class="nav-links">
+            <a href="//github.io/jo_geek" target="_blank">Forside</a>
+            <a href="http://stackoverflow.com/users/4084003/" target="_blank">Vinduer</a>
+            <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">Døre</a>
+            <a href="https://codepen.io/jo_Geek/" target="_blank">Galleri</a>
+            <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">Kontakt</a>
+        </div>
+    </div>
