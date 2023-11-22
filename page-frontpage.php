37094fc22c0021a32fb91c2978875545e4d080a7
Tanya <tanhan01@easv365.dk>
2023-11-22 14:05:12 +0100

    Update page-frontpage.php

--- page-frontpage.php
+++ page-frontpage.php
@@ -13,7 +13,7 @@
 <div class="frontpage-content">
   <div class="text-box">
     <h2 class="company-name">Avantgardevinduer & Døre</h2>
-    <p class="company-description">Firma beskrivelse</p>
+    <p class="company-description">Avantgardevinduer leverer lavenergi og superlavenergi (dvs. 3 lag glas) vinduer og døre med varm kant i standard og speciel mål af bedste kvalitet og til konkurrencedygtige priser. Kan leveres til egen montering, eller vi sørger for montering udført af professionelle håndværkere. <br><br> Vinduerne kan bestilles med 2 eller 3 lag glas i standard og special mål uden extra omkostninger. <br><br> Kontakt os, og vi kommer og foretager en korrekt opmåling, tilbyder rådgivning og udarbejder et uforpligtende tilbud.</p>
   </div>
   <div class="image-box">
     <img src="https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg" alt="">
