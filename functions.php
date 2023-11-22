25c745d4e6aa373d4c85a96dcef5ce657505ecec
Tanya <tanhan01@easv365.dk>
2023-11-22 10:12:46 +0100

    Theme push

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function enqueue_parent_theme_style()
+{
+    wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
+
