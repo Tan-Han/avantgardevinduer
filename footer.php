e1dde9aebfdbfad6601f065b8810520ed7818ba5
Tanya <tanhan01@easv365.dk>
2023-11-22 14:30:40 +0100

    Footer created and styled

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+<footer>
+  <div class="links">
+    <a href="">Forside</a>
+    <a href="">Døre</a>
+    <a href="">Vinduer</a>
+    <a href="">Galleri</a>
+    <a href="">Levering</a>
+    <a href="">Kontakt</a>
+  </div>
+  <div class="info">
+    <b>Avantgardevinduer</b>
+    <p>Fasanvej 15, 6760 Ribe</p>
+    <p>+45 20 87 23 32</p>
+    <p>bsh@avantgardevinduer.dk</p>
+    <p>CVR - 33019572</p>
+    <i class="fa fa-facebook-square"></i>
+  </div>
+  <div class="logo-box">
+  <img class="logo" src="https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg" alt="">
+  </div>
+</footer>
+
 <?php wp_footer() ?>
     
 </body>
