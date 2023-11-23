132da305a4b127ca092a1c2166dafb7b59264eaa
Tanya <tanhan01@easv365.dk>
2023-11-23 12:43:11 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
     wp_enqueue_style("adobe", "https://use.typekit.net/tmr5ico.css");
     wp_enqueue_style("cloudfare", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 
-    wp_enqueue_style('material-symbols', "fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200");
+    wp_enqueue_style('material-symbols', "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0");
   }
 
   add_action( "wp_enqueue_scripts", "mt_register_stylesheets" );
