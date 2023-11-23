7948fba4aa773bd64f6f368b7c6a6818bbb28b7d
Lærke Nielsen <laenie01@easv365.dk>
2023-11-23 15:05:33 +0100

    style

--- single-vindue.php
+++ single-vindue.php
@@ -77,7 +77,7 @@
             <?php $farver = get_field("farver") ?>
             <?php foreach($farver as $post):
                 setup_postdata($post) ?>
-                <div class="farver">
+                <div style="display: flex; gap: 18px;">
                     <div style="background-color: <?php the_field("hex_color") ?>; width: 50px; height: 50px;"></div>
                     <span><?php the_title() ?></span>
                 </div>
