178dcf20b3e55cf6f38fae162fb1cb1dba2f031e
Tanya <tanhan01@easv365.dk>
2023-11-23 10:12:55 +0100

    Update header.php

--- header.php
+++ header.php
@@ -6,94 +6,111 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.typekit.net/tmr5ico.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Avantgardevinduer & Døre</title>
     <?php wp_head() ?>
 </head>
 
 <body>
     <nav>
-        <div class="navbar">
-            <i class='bx bx-menu'></i>
-            <div class="logo"><a href="#">Avantgardevinduer</a></div>
-            <div class="nav-links">
-                <div class="sidebar-logo">
-                    <span class="logo-name">Hello</span>
-                    <i class='bx bx-x'></i>
-                </div>
-                <ul class="links">
-                    <li><a href="#">Forside</a></li>
-                    <li>
-                        <a href="#">Produkter</a>
-                        <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
-                        <ul class="htmlCss-sub-menu sub-menu">
-                            <li><a href="#">Vinduer</a></li>
-                            <li><a href="#">Døre</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Kontakt</a></li>
-                    <li><a href="#">Om os</a></li>
-                </ul>
-            </div>
+        <h2 class="logo">ASMR2Dev</h2>
+        <div class="bars">
+            <i class="fa fa-bars"></i>
         </div>
+        <ul class="nav-links">
+            <li class="nav-link"><a href="#">Home</a></li>
+            <li class="nav-link"><a href="#">Produkter</a>
+                <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+                <ul class="htmlCss-sub-menu sub-menu">
+                    <li><a href="#">Vinduer</a></li>
+                    <li><a href="#">Døre</a></li>
+                </ul>
+            </li>
+            <li class="nav-link"><a href="#">Contact</a></li>
+            <li class="nav-link auth"><a href="#">Register</a></li>
+            <li class="nav-link auth"><a href="#">Login</a></li>
+        </ul>
     </nav>
+
+
+
 
     <style>
         nav {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            height: 70px;
-            background: #333333;
-            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-            z-index: 99;
-        }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: white;
+  position: relative;
+}
 
-        nav .navbar {
-            height: 100%;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            margin: auto;
-            padding: 0 50px;
-        }
+/* responsive */
+nav.active {
+  flex-direction: column;
+  align-items: flex-start;
+}
 
-        .navbar .logo a {
-            font-size: 30px;
-            color: #fff;
-            text-decoration: none;
-            font-weight: 600;
-        }
+nav h2.logo {
+  font-weight: bold;
+  font-size: xx-large;
+}
 
-        nav .navbar .nav-links {
-            line-height: 70px;
-            height: 100%;
-        }
+nav .bars {
+  position: absolute;
+  right: 1rem;
+  font-size: xx-large;
+  display: none;
+}
 
-        nav .navbar .links {
-            display: flex;
-        }
+ul.nav-links {
+  display: flex;
+}
 
-        nav .navbar .links li {
-            position: relative;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            list-style: none;
-            padding: 0 14px;
-        }
+/* responsive */
+nav.active ul.nav-links {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
 
-        nav .navbar .links li a {
-            height: 100%;
-            text-decoration: none;
-            white-space: nowrap;
-            color: #fff;
-            font-size: 15px;
-            font-weight: 500;
-        }
+li.nav-link {
+  list-style: none;
+  margin: 0.5rem 1rem;
+  padding: 0.4rem;
+  display: flex;
+  align-items: center;
+}
+
+li.nav-link:hover {
+  background-color: #ccc;
+  color: #333;
+}
+
+li.nav-link.auth {
+  border: 2px solid white;
+  border-radius: 1rem;
+}
+
+li.nav-link a {
+  text-decoration: none;
+  color: inherit;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: large;
+  text-transform: uppercase;
+}
+
+@media screen and (max-width: 800px) {
+  nav .bars {
+    display: block;
+  }
+
+  nav .nav-links {
+    display: none;
+  }
+}
 
         .links li:hover .htmlcss-arrow,
         .links li:hover .js-arrow {
@@ -110,221 +127,10 @@
             transition: all 0.3s ease;
         }
 
-        nav .navbar .links li .sub-menu {
-            position: absolute;
-            top: 70px;
-            left: 0;
-            line-height: 40px;
-            background: #3E8DA8;
-            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-            border-radius: 0 0 4px 4px;
-            display: none;
-            z-index: 2;
-        }
-
-        nav .navbar .links li:hover .htmlCss-sub-menu,
-        nav .navbar .links li:hover .js-sub-menu {
-            display: block;
-        }
-
-        .navbar .links li .sub-menu li {
-            padding: 0 22px;
-            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-        }
-
-        .navbar .links li .sub-menu a {
-            color: #fff;
-            font-size: 15px;
-            font-weight: 500;
-        }
-
-        .navbar .links li .sub-menu .more-arrow {
-            line-height: 40px;
-        }
-
-        .navbar .links li .htmlCss-more-sub-menu {
-            /* line-height: 40px; */
-        }
-
-        .navbar .links li .sub-menu .more-sub-menu {
-            position: absolute;
-            top: 0;
-            left: 100%;
-            border-radius: 0 4px 4px 4px;
-            z-index: 1;
-            display: none;
-        }
-
-        .links li .sub-menu .more:hover .more-sub-menu {
-            display: block;
-        }   
-
-        .navbar .nav-links .sidebar-logo {
-            display: none;
-        }
-
-        .navbar .bx-menu {
-            display: none;
-        }
-
-        @media (max-width:920px) {
-            nav .navbar {
-                max-width: 100%;
-                padding: 0 25px;
-            }
-
-            nav .navbar .logo a {
-                font-size: 27px;
-            }
-
-            nav .navbar .links li {
-                padding: 0 10px;
-                white-space: nowrap;
-            }
-
-            nav .navbar .links li a {
-                font-size: 15px;
-            }
-        }
-
-        @media (max-width:800px) {
-            nav {
-                /* position: relative; */
-            }
-
-            .navbar .bx-menu {
-                display: block;
-            }
-
-            nav .navbar .nav-links {
-                position: fixed;
-                top: 0;
-                left: -100%;
-                display: block;
-                max-width: 270px;
-                width: 100%;
-                background: #3E8DA8;
-                line-height: 40px;
-                padding: 20px;
-                box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-                transition: all 0.5s ease;
-                z-index: 1000;
-            }
-
-            .navbar .nav-links .sidebar-logo {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-            }
-
-            .sidebar-logo .logo-name {
-                font-size: 25px;
-                color: #fff;
-            }
-
-            .sidebar-logo i,
-            .navbar .bx-menu {
-                font-size: 25px;
-                color: #fff;
-            }
-
-            nav .navbar .links {
-                display: block;
-                margin-top: 20px;
-            }
-
-            nav .navbar .links li .arrow {
-                line-height: 40px;
-            }
-
-            nav .navbar .links li {
-                display: block;
-            }
-
-            nav .navbar .links li .sub-menu {
-                position: relative;
-                top: 0;
-                box-shadow: none;
-                display: none;
-            }
-
-            nav .navbar .links li .sub-menu li {
-                border-bottom: none;
-            }
-
-            .navbar .links li .sub-menu .more-sub-menu {
-                display: none;
-                position: relative;
-                left: 0;
-            }
-
-            .navbar .links li .sub-menu .more-sub-menu li {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-            }
-
-            .links li:hover .htmlcss-arrow,
-            .links li:hover .js-arrow {
-                transform: rotate(0deg);
-            }
-
-            .navbar .links li .sub-menu .more-sub-menu {
-                display: none;
-            }
-
-            .navbar .links li .sub-menu .more span {
-                /* background: red; */
-                display: flex;
-                align-items: center;
-                /* justify-content: space-between; */
-            }
-
-            .links li .sub-menu .more:hover .more-sub-menu {
-                display: none;
-            }
-
-            nav .navbar .links li:hover .htmlCss-sub-menu,
-            nav .navbar .links li:hover .js-sub-menu {
-                display: none;
-            }
-
-            .navbar .nav-links.show1 .links .htmlCss-sub-menu,
-            .navbar .nav-links.show3 .links .js-sub-menu,
-            .navbar .nav-links.show2 .links .more .more-sub-menu {
-                display: block;
-            }
-
-            .navbar .nav-links.show1 .links .htmlcss-arrow,
-            .navbar .nav-links.show3 .links .js-arrow {
-                transform: rotate(180deg);
-            }
-
-            .navbar .nav-links.show2 .links .more-arrow {
-                transform: rotate(90deg);
-            }
-        }
-
-        @media (max-width:370px) {
-            nav .navbar .nav-links {
-                max-width: 100%;
-            }
-        }
+       
     </style>
 
     <script>
-
-        // sidebar open close js code
-        let navLinks = document.querySelector(".nav-links");
-        let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-        let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-        menuOpenBtn.onclick = function () {
-            navLinks.style.left = "0";
-        }
-        menuCloseBtn.onclick = function () {
-            navLinks.style.left = "-100%";
-        }
-
 
         // sidebar submenu open close js code
         let htmlcssArrow = document.querySelector(".htmlcss-arrow");
