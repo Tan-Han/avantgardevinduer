584847f51a76e32fd9cff58202cdebb274fb9456
Tanya <tanhan01@easv365.dk>
2023-11-23 19:55:45 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,22 +10,26 @@
     <p class="bold">Avantgardevinduer</p>
     <p>Fasanvej 15, 6760 Ribe</p>
     <a href="<?php the_field("telephone") ?>">
-            <?php the_field("telephone") ?>
-        </p></a>
+      <p>
+        <?php the_field("telephone") ?>
+      </p>
+    </a>
     <a href="<?php the_field("mail") ?>">
-          <p>
-            <?php the_field("mail") ?>
-          </p>
-        </a>
+      <p>
+        <?php the_field("mail") ?>
+      </p>
+    </a>
     <p>CVR - 33019572</p>
-    <a class="fb-link" href="https://www.facebook.com/avantgardevinduer" target=”_blank”><i class="fa fa-facebook-square"></i></a>
+    <a class="fb-link" href="https://www.facebook.com/avantgardevinduer" target=”_blank”><i
+        class="fa fa-facebook-square"></i></a>
   </div>
   <div class="logo-box">
-  <img class="logo" src="https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg" alt="">
+    <img class="logo" src="https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg" alt="">
   </div>
 </footer>
 
 <?php wp_footer() ?>
-    
+
 </body>
+
 </html>
