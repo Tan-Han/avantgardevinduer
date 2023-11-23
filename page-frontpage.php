095871ac5ddfee2c33b78501910882f4cf95d2c2
Tanya <tanhan01@easv365.dk>
2023-11-23 09:14:27 +0100

    Frontpage style moved to page-frontpage.php

--- page-frontpage.php
+++ page-frontpage.php
@@ -13,7 +13,11 @@
 <div class="frontpage-content">
   <div class="text-box">
     <h2 class="company-name">Avantgardevinduer & Døre</h2>
-    <p class="company-description">Avantgardevinduer leverer lavenergi og superlavenergi (dvs. 3 lag glas) vinduer og døre med varm kant i standard og speciel mål af bedste kvalitet og til konkurrencedygtige priser. Kan leveres til egen montering, eller vi sørger for montering udført af professionelle håndværkere. <br><br> Vinduerne kan bestilles med 2 eller 3 lag glas i standard og special mål uden extra omkostninger. <br><br> Kontakt os, og vi kommer og foretager en korrekt opmåling, tilbyder rådgivning og udarbejder et uforpligtende tilbud.</p>
+    <p class="company-description">Avantgardevinduer leverer lavenergi og superlavenergi (dvs. 3 lag glas) vinduer og
+      døre med varm kant i standard og speciel mål af bedste kvalitet og til konkurrencedygtige priser. Kan leveres til
+      egen montering, eller vi sørger for montering udført af professionelle håndværkere. <br><br> Vinduerne kan
+      bestilles med 2 eller 3 lag glas i standard og special mål uden extra omkostninger. <br><br> Kontakt os, og vi
+      kommer og foretager en korrekt opmåling, tilbyder rådgivning og udarbejder et uforpligtende tilbud.</p>
   </div>
   <div class="image-box">
     <img src="https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg" alt="">
@@ -21,8 +25,80 @@
 </div>
 
 <style>
+  .hero {
+    background-image: url(https://avantgardevinduer.trshansen.com/wp-content/uploads/2023/11/IMG_3700.jpg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 685px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
   h1 {
-    color: red;
+    padding: 1rem 0;
+    color: #f5f5f5;
+    font-size: 85px;
+  }
+
+  h3 {
+    color: #f5f5f5;
+    padding: 1rem 0;
+    font-size: 60px;
+    font-weight: 400;
+  }
+
+  hr {
+    color: #f5f5f5;
+    width: 60%;
+    border-top: 3px solid;
+  }
+
+  .frontpage-mini-nav {
+    width: 100%;
+    height: 60px;
+    background-color: #708090;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .frontpage-mini-nav p {
+    color: #f5f5f5;
+    font-size: 25px;
+    font-weight: 700;
+  }
+
+  .frontpage-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .frontpage-content .text-box {
+    width: 50%;
+    padding: 100px 140px;
+  }
+
+  .frontpage-content .text-box h2 {
+    font-size: 50px;
+  }
+
+  .frontpage-content .text-box p {
+    font-size: 20px;
+  }
+
+  .frontpage-content .image-box {
+    width: 50%;
+    padding: 100px 140px;
+  }
+
+  .frontpage-content .image-box img {
+    max-width: 100%;
   }
 </style>
 
