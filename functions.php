f42d0680090db7bf60558ca5a6fc33ccde0595cb
Lærke Nielsen <laenie01@easv365.dk>
2023-11-23 10:22:30 +0100

    Fixed BS

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
   function mt_register_stylesheets() {
     wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
-    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
-    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
+    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
+    wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
   }
   add_action( "wp_enqueue_scripts", "mt_register_stylesheets" );
   
