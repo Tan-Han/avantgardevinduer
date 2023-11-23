0e832267db76d4960a755fdbbd4efb17d99b1732
Lærke Nielsen <laenie01@easv365.dk>
2023-11-23 09:20:29 +0100

    added bootstrap

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
   function mt_register_stylesheets() {
     wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
+    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
+    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
   }
   add_action( "wp_enqueue_scripts", "mt_register_stylesheets" );
