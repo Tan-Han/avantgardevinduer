d4f8083bc95ceb475ca917cc743c81c2b2bd4dbd
Tanya <tanhan01@easv365.dk>
2023-11-23 10:28:03 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
   function mt_register_stylesheets() {
     wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
-    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
-    wp_enqueue_style("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
+    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
+    wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
 
-    wp_enqueue_style("https://use.typekit.net/tmr5ico.css");
-    wp_enqueue_style("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+    wp_enqueue_style("adobe", "https://use.typekit.net/tmr5ico.css");
+    wp_enqueue_style("cloudfare", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
   }
   add_action( "wp_enqueue_scripts", "mt_register_stylesheets" );
   
