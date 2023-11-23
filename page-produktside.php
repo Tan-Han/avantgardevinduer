17c500b11d045e0d38b76d377ab373d24ab9189f
Lærke Nielsen <laenie01@easv365.dk>
2023-11-23 09:49:37 +0100

    added html of produktside

---
+++ page-produktside.php
@@ -0,0 +1,168 @@
+<?php get_header() ?>
+
+<!-- hero -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <!-- titel -->
+            <div>
+                <h2>Plastvindue</h2>
+                <p>Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur Lorem ipsum dolor sit amet, consetetur </p>
+            </div>
+
+            <!-- specifikationer -->
+            <div class="row">
+                <div class="col">
+                    <div>
+                        <hr>
+                        <h4>Overskrift</h4>
+                        <p>Text</p>
+                    </div>
+                    <div>
+                        <hr>
+                        <h4>Overskrift</h4>
+                        <p>Text</p>
+                    </div>
+                    
+                </div>
+                <div class="col">
+                    <div>
+                        <hr>
+                        <h4>Overskrift</h4>
+                        <p>Text</p>
+                    </div>
+                    <div>
+                        <hr>
+                        <h4>Overskrift</h4>
+                        <p>Text</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+        <!-- img -->
+        <div class="col">
+            <div>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- farver -->
+<div>
+    <hr>
+    <h3>Farver</h3>
+    <p>GEALAN offers a wide range of options for bringing colour to PVC windows. One of them is the proven surface technology GEALAN-acrylcolor®. The coloured acrylic layer is scratch-resistant, weather resistant and easy-care. That means that window systems with GEALAN-acrylcolor® meet the highest technical and functional demands for innovative, coloured surfaces. There are also a wealth of options for creating coloured window and door profiles with GEALAN’s extensive range of decorative foils. The standard range offers a wide spectrum of colours. Additional tones and textures are available upon request on a property-related basis.</p>
+</div>
+
+<!-- loop med farver -->
+<div class="container">
+    <div class="row">
+        <h4>Navn på farve</h4>
+        <img src="" alt="">
+        <p>Farve nummer</p>
+    </div>
+</div>
+
+<!-- benefits -->
+<div>
+    <div>
+        <h3>Benefits of the S 9000 window system</h3>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <!-- billede -->
+            <div class="col">
+                <img src="" alt="">
+            </div>
+
+            <!-- information -->
+            <div class="col">
+                <hr>
+                <h3>h3</h3>
+                <!-- loop med teksten omkring fordelene -->
+                <div>
+                    <h4>h4</h4>
+                    <p>p</p>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <!-- billede -->
+            <div class="col">
+                <hr>
+                <h3>h3</h3>
+                <!-- loop med teksten omkring fordelene -->
+                <div>
+                    <h4>h4</h4>
+                    <p>p</p>
+                </div>
+            </div>
+
+            <!-- information -->
+            <div class="col">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <hr>
+                <h3>GEALAN-LUMAXX®</h3>
+                <h4>Optimised geometries</h4>
+                <p>During development of GEALAN-LUMAXX®, the entire frame geometry was optimised while the profile widths were compressed to a minimum. The designers paid special attention to achieving a balanced relationship between insulating properties and statics, as well as a perfect interplay of components. The symmetrical effect of the narrow single mullion also adds to the flexibility of use in window construction.</p>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Visibly less frame</h3>
+                <p>A direct comparison proves it: The new GEALAN-LUMAXX® is significantly narrower than previous frame combinations. In the past, mullion designs often required a width of up to 150 mm and above. Heavy, unattractive bars ruined the window outline and let far less light into the room. Slim sightlines can finally be achieved with the new system, without forgoing ease-of-use and security.</p>
+            </div>  
+            <div class="col">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="" alt="">
+            </div>
+            <div class="col">
+                <h3>No compromise</h3>
+                <p>Despite the focus on narrow sightlines, the new sash/mullion profile doesn’t compromise when it comes to statics. Both use a particular steel which, due to its static properties, allows for elements of up to 2.30 m in white and 2.20 m in colour. In combination with STV® bonding technology, element sizes up to 2.50 m in white and 2.40 m in colour are possible.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <hr>
+                <h3>GEALAN-LUMAXX®</h3>
+                <h4>Optimised geometries</h4>
+                <p>During development of GEALAN-LUMAXX®, the entire frame geometry was optimised while the profile widths were compressed to a minimum. The designers paid special attention to achieving a balanced relationship between insulating properties and statics, as well as a perfect interplay of components. The symmetrical effect of the narrow single mullion also adds to the flexibility of use in window construction.</p>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php get_footer() ?>
