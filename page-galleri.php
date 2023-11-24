fba5080cbfd7538ce0fc06dfe20e1f9f99a8ea7d
Lærke Nielsen <laenie01@easv365.dk>
2023-11-24 14:52:46 +0100

    carousel

--- page-galleri.php
+++ page-galleri.php
@@ -1,10 +1,9 @@
 <?php get_header() ?>
 
 <!-- galleri slider -->
-<div class="mr-5 ml-5">
+<div class="pt-5 pb-5">
     <?php echo do_shortcode('[sp_wpcarousel id="149"]'); ?>
-</div>  
-<div class="pb-5"></div>
+</div>
 
 
 
