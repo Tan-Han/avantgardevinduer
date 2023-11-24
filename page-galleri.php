697375c411b0e80d7c42a6bcf9a8c42b6d403f2a
Lærke Nielsen <laenie01@easv365.dk>
2023-11-24 14:57:33 +0100

    carousel

--- page-galleri.php
+++ page-galleri.php
@@ -2,7 +2,7 @@
 
 <h1 class="pt-5 text-center">Galleri - Projekter</h1>
 <!-- galleri slider -->
-<div class="p-5">
+<div class="p-5 pt-3">
     <?php echo do_shortcode('[sp_wpcarousel id="149"]'); ?>
 </div>
 
