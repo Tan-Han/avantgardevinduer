83587becad83cb1b5eb230dc42cbf267b0a8ff1d
Tanya <tanhan01@easv365.dk>
2023-11-25 11:52:23 +0100

    Update page-vinduer.php

--- page-vinduer.php
+++ page-vinduer.php
@@ -1,8 +1,32 @@
 <?php get_header() ?>
 
+<div class="headline-productlist">
+<div class="about-headline">
+  <h1>Vinduer</h1>
+  <h2>Her kan du læse mere om de vinduer vi tilbyder</h2>
+
+  <hr>
+</div>
+</div>
+
+<div class="content-productlist">
     <?php $productLoop = new WP_Query(array("post_type" => "vindue", "posts_per_page" => -1)) ?>
-    <?php while($productLoop->have_posts()): $productLoop->the_post() ?>
-        <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+    <?php while ($productLoop->have_posts()):
+        $productLoop->the_post() ?>
+
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <a href="<?php the_permalink() ?>"><h3><?php the_title() ?></h3></a>
+                <p class="card-text"><?php the_field("kort_beskrivelse") ?></p>
+            </div>
+        </div>
+
+        <a href="<?php the_permalink() ?>">
+            <?php the_title() ?>
+        </a>
     <?php endwhile ?>
+
+</div>
 
 <?php get_footer() ?>
