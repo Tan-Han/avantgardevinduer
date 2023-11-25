6cd94add57ae66ad69e76a97aac6e1d39ac9eae8
Tanya <tanhan01@easv365.dk>
2023-11-25 11:56:04 +0100

    Update page-vinduer.php

--- page-vinduer.php
+++ page-vinduer.php
@@ -1,12 +1,12 @@
 <?php get_header() ?>
 
 <div class="headline-productlist">
-<div class="about-headline">
-  <h1>Vinduer</h1>
-  <h2>Her kan du læse mere om de vinduer vi tilbyder</h2>
+    <div class="about-headline">
+        <h1>Vinduer</h1>
+        <h2>Her kan du læse mere om de vinduer vi tilbyder</h2>
 
-  <hr>
-</div>
+        <hr>
+    </div>
 </div>
 
 <div class="content-productlist">
@@ -17,13 +17,50 @@
         <div class="card" style="width: 18rem;">
             <img src="<?php the_field("produkt_billede") ?>" class="card-img-top" alt="...">
             <div class="card-body">
-                <a href="<?php the_permalink() ?>"><h3><?php the_title() ?></h3></a>
-                <p class="card-text"><?php the_field("kort_beskrivelse") ?></p>
+                <a href="<?php the_permalink() ?>">
+                    <h3>
+                        <?php the_title() ?>
+                    </h3>
+                </a>
+                <p class="card-text">
+                    <?php the_field("kort_beskrivelse") ?>
+                </p>
             </div>
         </div>
 
     <?php endwhile ?>
 
 </div>
+
+<style>
+    .headline-productlist {
+        text-align: center;
+        padding: 3rem 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .headline-productlist h1 {
+        font-size: 70px;
+        font-weight: 700;
+    }
+
+    .headline-productlist h2 {
+        font-size: 30px;
+        font-style: italic;
+    }
+
+    hr {
+        width: 50%;
+        border-top: 3px solid #333333;
+        opacity: 100%;
+    }
+
+    .content-productlist {
+        padding: 0 10% 3rem 10%;
+    }
+</style>
 
 <?php get_footer() ?>
