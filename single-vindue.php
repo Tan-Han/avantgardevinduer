003325f54ed503494fcb2591a49a4f9cfb8fcc5c
Lærke Nielsen <laenie01@easv365.dk>
2023-11-26 13:48:52 +0100

    responsive

--- single-vindue.php
+++ single-vindue.php
@@ -106,7 +106,7 @@
     </div>
 
     <!-- fordel 1 -->
-    <div class="container ten-percent responsive-col">
+    <div class="ten-percent responsive-col">
         <div class="flex fordel">
             <!-- billede -->
             <div class="flex-col">
