667240499741dd40e5200763a494295be7153730
Lærke Nielsen <laenie01@easv365.dk>
2023-11-27 17:49:16 +0100

    gap

--- single-dor.php
+++ single-dor.php
@@ -11,8 +11,8 @@
             </div>
  
             <!-- specifikationer -->
-            <div class="flex parameter">
-                <div class="flex-col w-half pb-5 parameter gap-18">
+            <div class="flex parameter gap-18">
+                <div class="flex-col w-half pb-5 parameter">
                     <div>
                         <hr class="hr-small">
                         <h5 class="pb-2"><?php the_field("parameter_titel_1")?></h5>
@@ -25,7 +25,7 @@
                     </div>
                     
                 </div>
-                <div class="flex-col w-half pb-5 parameter gap-18">
+                <div class="flex-col w-half pb-5 parameter">
                     <div>
                         <hr class="hr-small">
                         <h5 class="pb-2"><?php the_field("parameter_titel_3")?></h5>
