54d415910fbdf9ffd59ea26c2a3932fca9cc0b6a
Tanya <tanhan01@easv365.dk>
2023-11-28 13:59:12 +0100

    its a small one

--- page-vinduer.php
+++ page-vinduer.php
@@ -14,7 +14,7 @@
 
         <div class="card" style="width: 18rem;">
             <?php $image = get_field("produkt_billede") ?>
-            <img src="<?php echo $image["sizes"]["medium"] ?>" class="card-img-top" alt="...">
+            <img src="<?php echo $image["sizes"]["small"] ?>" class="card-img-top" alt="...">
             <div class="card-body">
                 <a href="<?php the_permalink() ?>">
                     <h3>
