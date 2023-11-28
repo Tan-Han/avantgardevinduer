fa5d53bb314876f4ffcde76f2f3210db3b3c093a
Lærke Nielsen <laenie01@easv365.dk>
2023-11-28 14:47:33 +0100

    added main

--- page-galleri.php
+++ page-galleri.php
@@ -1,16 +1,18 @@
 <?php get_header() ?>
 
-<div class="headline-galleri">
-    <h1>Galleri</h1>
-    <h2>Her kan du se billeder af projekter vi har udført</h2>
+<main>
+    <div class="headline-galleri">
+        <h1>Galleri</h1>
+        <h2>Her kan du se billeder af projekter vi har udført</h2>
 
-    <hr>
-</div>
+        <hr>
+    </div>
 
-<!-- galleri slider -->
-<div class="p-5 pt-3">
-    <?php echo do_shortcode('[sp_wpcarousel id="149"]'); ?>
-</div>
+    <!-- galleri slider -->
+    <div class="p-5 pt-3">
+        <?php echo do_shortcode('[sp_wpcarousel id="149"]'); ?>
+    </div>
 
+</main>
 
 <?php get_footer() ?>
